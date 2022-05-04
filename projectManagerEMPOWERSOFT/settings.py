d15aab528eef145acd6b0d5c3214cbf44f39b4de
brobert04 <robert.bercaru@liceuldearte.ro>
2022-05-04 14:59:42 +0200

    First Commit

--- projectManagerEMPOWERSOFT/settings.py
+++ projectManagerEMPOWERSOFT/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-)(rkd^@=86i(m&_0jr9-alwg2kaeu^x(xgd$#yd6(3hzclw*8c'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1']
 
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
 	 "django.contrib.sessions",
 	 "django.contrib.messages",
 	 "django.contrib.staticfiles",
-	 "projectManagerDjango"
+	 "projectManagerDjango",
 ]
 
 MIDDLEWARE = [
