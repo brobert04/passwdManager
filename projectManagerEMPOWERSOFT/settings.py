61fffea2116a3ceb478f7bddc0e0f7740efeb8ac
brobert04 <robert.bercaru@liceuldearte.ro>
2022-05-04 16:11:00 +0200

    Added host website!

--- projectManagerEMPOWERSOFT/settings.py
+++ projectManagerEMPOWERSOFT/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-)(rkd^@=86i(m&_0jr9-alwg2kaeu^x(xgd$#yd6(3hzclw*8c
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1', 'mykey2022.herokuapp.com']
 
 
 # Application definition
