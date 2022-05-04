8b3a1c2267f47ae61ee84438537d1fc0cfd3f8ec
brobert04 <robert.bercaru@liceuldearte.ro>
2022-05-04 15:03:42 +0200

    Second Commit

--- projectManagerEMPOWERSOFT/settings.py
+++ projectManagerEMPOWERSOFT/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-)(rkd^@=86i(m&_0jr9-alwg2kaeu^x(xgd$#yd6(3hzclw*8c'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1']
 
