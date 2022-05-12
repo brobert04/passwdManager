8ee4aa2de1462ebebe9cc1e7a85bb9ffd1c08185
brobert04 <robert.bercaru@liceuldearte.ro>
2022-05-12 16:32:53 +0300

    secret_key

--- projectManagerEMPOWERSOFT/settings.py
+++ projectManagerEMPOWERSOFT/settings.py
@@ -22,7 +22,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET-KEY')
+SECRET_KEY = os.getenv('SECRET_KEY')
+
 # SECURITY WARNING: don't run with debug turned on in production!
 
 DEBUG = True   
@@ -144,6 +145,16 @@ EMAIL_USE_TLS = True
 
 # SETARI PENTRU CRIPTAREA DATELOR UTILIZATORULUI
 KEY = b'_ymUHmKIuLPkpdQ2EcztwpKzwD1kpKRH8aID7IQOhZs='
+
+CORS_REPLACE_HTTPS_REFERER      = False
+HOST_SCHEME                     = "http://"
+SECURE_PROXY_SSL_HEADER         = None
+SECURE_SSL_REDIRECT             = False
+SESSION_COOKIE_SECURE           = False
+CSRF_COOKIE_SECURE              = False
+SECURE_HSTS_SECONDS             = None
+SECURE_HSTS_INCLUDE_SUBDOMAINS  = False
+SECURE_FRAME_DENY               = False
 
 django_heroku.settings(locals())
 
