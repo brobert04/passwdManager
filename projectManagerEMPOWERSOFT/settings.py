43cf23af72cd5486cd1cb65c0102cce911943c6e
brobert04 <robert.bercaru@liceuldearte.ro>
2022-05-12 10:37:42 +0300

    SECRET KEY HIDE

--- projectManagerEMPOWERSOFT/settings.py
+++ projectManagerEMPOWERSOFT/settings.py
@@ -22,8 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-)(rkd^@=86i(m&_0jr9-alwg2kaeu^x(xgd$#yd6(3hzclw*8c'
-
+SECRET_KEY = os.environ.get('SECRET-KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
